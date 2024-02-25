1f989b4e863e99e249c95a680bd05b73fb198b40
jafeth001 <jafethadet2021@gmail.com>
2024-02-26 01:06:28 +0300

    second commit

--- src/main/java/com/zeraki/projectAssessment/entity/Institution.java
+++ src/main/java/com/zeraki/projectAssessment/entity/Institution.java
@@ -10,7 +10,6 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 @Data
 @Entity
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Institution {
--- src/main/java/com/zeraki/projectAssessment/entity/Student.java
+++ src/main/java/com/zeraki/projectAssessment/entity/Student.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
